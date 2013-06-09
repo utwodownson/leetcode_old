101f2c44100fcade816f3672e6de888aeb469167
Charles Yang <utwodownson@gmail.com>
2013-06-09 14:36:07 +0800

    06/09/13

--- 126.Reverse_Integer.cpp
+++ 126.Reverse_Integer.cpp
@@ -7,21 +7,14 @@ class Solution {
         int reverse(int x) {
             // Start typing your C/C++ solution below
             // DO NOT write int main() function
-            if (x < 10 && x > -10) return x;
-
-            char str[20];
-            int i = 0, tmp = abs(x), result = 0;
-
-            while (tmp) {
-                str[i++] = tmp % 10 + '0';
-                tmp /= 10;
+            int sign = x < 0 ? -1 : 1;
+            x = abs(x);
+            int y = 0;
+            while (x > 0) {
+                y = y * 10 + x % 10;
+                x /= 10;
             }
-
-            for (int j = 0; j < i; ++j) 
-                result = result * 10 + str[j] - '0'; 
-
-            if (x > 0) return result;
-            else       return 0 - result;
+            return y * sign;
         }
 };
 
