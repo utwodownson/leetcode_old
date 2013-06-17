1d8a4a89ca2546e18a64ac203182bb3e8dfc0d02
utwodownson <utwodownson@gmail.com>
2013-06-18 00:10:47 +0800

    06/18/13

--- 131.Median_of_Two_Sorted.cpp
+++ 131.Median_of_Two_Sorted.cpp
@@ -1,18 +1,41 @@
 class Solution {
-    public:
-        double findMedianSortedArrays(int A[], int m, int B[], int n) {
-                    // Start typing your C/C++ solution below
-                    // DO NOT write int main() function
-
-                    int *C = (int *) malloc(sizeof(int) * (m + n));
-                    int i = 0, left = 0, right = 0;
-                    while (i < (m + n) && left < m && right < n) 
-                        C[i++] = A[left] > B[right] ? B[right++] : A[left++];
-                    while (left < m)
-                        C[i++] = A[left++];
-                    while (right < n)
-                        C[i++] = B[right++];
-                    int mid = m - (m - n) / 2;
-                    return C[mid];
+public:
+    int findK(int A[], int m, int B[], int n, int k) {
+        while (k > 1 && m > 0 && n > 0) {
+            int ka = k * m / (m + n);
+            int kb = k * n / (m + n);
+            if (ka + kb < k) {
+                if (ka < kb) ka++;
+                else kb++;
+            }
+            if (A[ka - 1] < B[kb - 1]) {
+                A += ka;
+                m -= ka;
+                n = kb;
+                k -= ka;
+            } else if (A[ka - 1] > B[kb - 1]) {
+                B += kb;
+                n -= kb;
+                m = ka;
+                k -= kb;
+            } else {
+                return A[ka - 1];
+            }
         }
+        if (m > 0 && n > 0) {
+            return min(A[0], B[0]);
+        } else if (m > 0) {
+            return A[k - 1];
+        } else if (n > 0) {
+            return B[k - 1];
+        } else {
+            return 0;
+        }
+    }
+    
+    double findMedianSortedArrays(int A[], int m, int B[], int n) {        
+        int k1 = (m + n + 1) / 2, k2 = (m + n) / 2 + 1;
+        return (findK(A, m, B, n, k1) + findK(A, m, B, n, k2)) / 2.0;    
+    }
 };
+
