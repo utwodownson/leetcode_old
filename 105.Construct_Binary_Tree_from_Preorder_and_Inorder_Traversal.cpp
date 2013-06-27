1d360273c1efe2bea5e1d8111a832d732ca7313f
Charles Yang <utwodownson@gmail.com>
2013-06-27 16:21:14 +0800

    06/27/13

--- 105.Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal.cpp
+++ 105.Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal.cpp
@@ -14,11 +14,27 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
-class Solution {
-    public:
-        TreeNode *buildTree(vector<int> &preorder, vector<int> &inorder) {
-            // Start typing your C/C++ solution below
-            // DO NOT write int main() function
 
+class Solution {
+    TreeNode *buildTree(const int *pre, const int *in, int size) {
+        if (size < 1) {
+            return 0;
         }
+        int val = pre[0];
+        TreeNode *root = new TreeNode(val);
+        for (int i = 0; i < size; i++) {
+            if (in[i] == val) {
+                root->left = buildTree(pre+1, in, i);
+                root->right = buildTree(pre + i + 1, in + i + 1, size - i - 1);
+                break;
+            }
+        }
+        return root;
+    }
+    public:
+    TreeNode *buildTree(vector<int> &preorder, vector<int> &inorder) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        return buildTree(preorder.data(), inorder.data(), preorder.size());
+    }
 };
--- 106.Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal.cpp
+++ 106.Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal.cpp
@@ -14,11 +14,28 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
-class Solution {
-    public:
-        TreeNode *buildTree(vector<int> &inorder, vector<int> &postorder) {
-            // Start typing your C/C++ solution below
-            // DO NOT write int main() function
 
+class Solution {
+    TreeNode *buildTree(const int *in, const int *post, int size) {
+        if (size < 1) {
+            return 0;
         }
+        int val = post[size - 1];
+        TreeNode *root = new TreeNode(val);
+        for (int i = 0; i < size; i++) {
+            if (in[i] == val) {
+                root->left = buildTree(in, post, i);
+                root->right = buildTree(in + i + 1, post + i, size - i - 1); // woring post + i + 1
+                break;
+            }
+        }
+        return root;
+    }
+    public:
+    TreeNode *buildTree(vector<int> &inorder, vector<int> &postorder) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        return buildTree(inorder.data(), postorder.data(), postorder.size());
+    }
 };
+
