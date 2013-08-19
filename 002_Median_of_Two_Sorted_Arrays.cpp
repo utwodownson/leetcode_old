7926b6e546b54078222552650969c864b67181ae
utwodownson <utwodownson@gmail.com>
2013-08-19 23:40:01 +0800

    2013/08/19  23:39:48

---
+++ 002_Median_of_Two_Sorted_Arrays.cpp
@@ -0,0 +1,16 @@
+/* There are two sorted arrays A and B of size m and n respectively. 
+ * Find the median of the two sorted arrays. 
+ * The overall run time complexity should be O(log (m+n)).
+ */
+class Solution {
+    int findK(int A[], int m, int B[], int n, int k) {
+        while (k > 1 && m > 0 && n > 0) {
+        }
+    }
+    public:
+        double findMedianSortedArrays(int A[], int m, int B[], int n) {
+            // Start typing your C/C++ solution below
+            // DO NOT write int main() function
+                
+        }
+};
