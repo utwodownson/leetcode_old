ef9bcb5df156ebdd25acf092095738be86aedce7
Charles Yang <utwodownson@gmail.com>
2013-08-26 16:20:19 +0800

    2013/08/26  16:20:11

--- 010_Regular_Expression_Matching.cpp
+++ 010_Regular_Expression_Matching.cpp
@@ -18,3 +18,12 @@
  * isMatch("ab", ".*") ? true
  * isMatch("aab", "c*a*b") ? true
  */
+
+class Solution {
+    public:
+        bool isMatch(const char *s, const char *p) {
+            // Start typing your C/C++ solution below
+            // DO NOT write int main() function    
+            
+        }
+};
