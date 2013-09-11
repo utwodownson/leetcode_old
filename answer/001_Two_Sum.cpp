6d424ef37638cbe2735c66371921ea1c42bca0de
utwodownson <utwodownson@gmail.com>
2013-09-11 23:23:27 +0800

    2013/09/11  23:23:13

--- answer/001_Two_Sum.cpp
+++ answer/001_Two_Sum.cpp
@@ -1,10 +1,10 @@
 /* Given an array of integers, find two numbers such that they add up to a specific target number.
- * 
- * The function twoSum should return indices of the two numbers such that they add up to the target, 
+ *
+ * The function twoSum should return indices of the two numbers such that they add up to the target,
  * where index1 must be less than index2. Please note that your returned answers (both index1 and index2) are not zero-based.
- * 
+ *
  * You may assume that each input would have exactly one solution.
- * 
+ *
  * Input: numbers = { 2, 7, 11, 15 }, target = 9
  * Output: index1 = 1, index2 = 2
  */
@@ -12,7 +12,7 @@
 typedef struct _Node {
     int val;
     int index;
-    _Node(){} // 
+    _Node(){} //
     _Node(int v, int idx) : val(v), index(idx){} //
 }Node;
 
@@ -28,11 +28,11 @@ class Solution {
             vector<Node> list;
             for (int i = 0 ; i < numbers.size(); ++i)  // size() list
                 list.push_back(Node(numbers[i], i + 1)); // push_back
-                
+
             sort(list.begin(), list.end(), cmp); // sort
-            
+
             vector<int> ret;
-            int i = 0, j = numbers.size() - 1; 
+            int i = 0, j = numbers.size() - 1;
             while (i < j) {
                 int sum = list[i].val + list[j].val;
                 if (sum < target) ++i;
