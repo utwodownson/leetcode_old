bd48efa3a10f72d142f12ef7ddcda0bf427781ad
Charles Yang <utwodownson@gmail.com>
2013-10-14 16:20:27 +0800

    work down in aisino a bad day

--- Count_and_Say.cpp
+++ Count_and_Say.cpp
@@ -10,6 +10,6 @@ class Solution {
     public:
         string countAndSay(int n) {
             // Note: The Solution object is instantiated only once and is reused by each test case.
-            
+                         
         }
 };
