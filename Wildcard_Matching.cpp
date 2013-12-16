16bcb4dd344100cf2ce9a6bf820944f49e148c85
Charles <utwodownson@gmail.com>
2013-12-16 17:08:55 +0800

    2013/12/16  17:08:47+in aisino

--- Wildcard_Matching.cpp
+++ Wildcard_Matching.cpp
@@ -6,29 +6,21 @@ class Solution {
             // the same Solution instance will be reused for each test case.
             bool flag = false;
             const char *str, *ptr;
-
-            for (str = s, ptr = p; *str != '\0'; str++, ptr++) {
-                switch (*ptr) {
-                    case '?' :
-                        break;
-                    case '*' :
-                        flag = true;
-                        s = str, p = ptr;
-                        while (*p == '*') p++;
-                        if (*p == '\0') return true;
-                        str = s - 1;
-                        ptr = p - 1;
-                        break;
-                    default :
-                        if (*str != *ptr) {
-                            if (!flag) return false;
-                            s++;
-                            str = s - 1;
-                            ptr = p - 1;
-                        }
-                }
+            for (str = s, ptr = p; *str != '\0'; ++str, ++ptr) {
+                if (*ptr == '?') continue;
+                else if (*ptr == '*') {
+                    flag = true;
+                    for (s = str, p = ptr; *ptr == '*'; ++ptr, ++p);
+                    if (*ptr == '\0') return true;
+                    --str; --ptr;
+                } else if (*str != *ptr) {
+                    if (!flag) return false;
+                    ++s;
+                    str = s - 1;
+                    ptr = p - 1;
+                } 
             }
-            while (*ptr == '*') ptr++;
-            return (*ptr == '\0');
+            for ( ; *ptr == '*'; ++ptr);
+            return *ptr == '\0';
         }
 };
