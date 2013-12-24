17d2954552c2b0b5415321eebd33f54169be21e7
Charles <utwodownson@gmail.com>
2013-12-24 17:21:30 +0800

    2013/12/24  17:21:21+in aisino

--- Merge_Two_Sorted_Lists.cpp
+++ Merge_Two_Sorted_Lists.cpp
@@ -13,32 +13,17 @@ class Solution {
     public:
         ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
             // Note: The Solution object is instantiated only once and is reused by each test case.
-            ListNode *head = 0, *p;
-            while (l1 && l2) {
+            ListNode *p = 0, *head = new ListNode(-1);
+            for (p = head; l1 && l2; p = p->next) { 
                 if (l1->val < l2->val) {
-                    if (head == 0) {
-                        head = p = l1;
-                        l1 = l1->next;
-                    } else {
-                        p->next = l1;
-                        p = l1;
-                        l1 = l1->next;
-                    }
+                    p->next = l1;
+                    l1 = l1->next;
                 } else {
-                    if (head == 0) {
-                        head = p = l2;
-                        l2 = l2->next;
-                    } else {
-                        p->next = l2;
-                        p = l2;
-                        l2 = l2->next;
-                    }
+                    p->next = l2;
+                    l2 = l2->next;
                 }
             }
-            if (l1) 
-                p->next = l1;
-            if (l2) 
-                p->next = l2;
-            return head;
+            p->next = l1 ? l1 : l2;
+            return head->next;
         }
 };
--- Merge_k_Sorted_Lists.cpp
+++ Merge_k_Sorted_Lists.cpp
@@ -1,52 +1,30 @@
 /* Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.*/
 
 class Solution {
-    public:
-        ListNode *mergeKLists(vector<ListNode *> &lists) {
-            // IMPORTANT: Please reset any member data you declared, as
-            // the same Solution instance will be reused for each test case.
-            ListNode *sorted = 0;
-            vector<ListNode*> heap(lists.size() + 1, 0);
-            int size = 0;
-
-            for (int i = 0; i < lists.size(); ++i) {
-                ListNode *key = lists[i];
-                if (key == 0) continue;
-
-                int j = size++, k = (j - 1) / 2;
-                while (j > 0 && heap[k]->val > key->val) {
-                    heap[j] = heap[k];
-                    j = k;
-                    k = (j - 1) / 2;
-                }
-                heap[j] = key;
+    ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        ListNode *p = 0, *head = new ListNode(-1);
+        for (p = head; l1 && l2; p = p->next) { 
+            if (l1->val < l2->val) {
+                p->next = l1;
+                l1 = l1->next;
+            } else {
+                p->next = l2;
+                l2 = l2->next;
             }
-
-            ListNode *key = 0, *p = 0;
-            if (size > 0) key = heap[0];
-            while (size > 0) {
-                int i = 0, j = 0;
-                do {
-                    heap[i] = heap[j];
-                    i = j;
-                    j = 2 * i + 1;
-                    if (j + 1 < size && heap[j]->val > heap[j + 1]->val) j++;
-                } while (j < size && key->val > heap[j]->val);
-
-                heap[i] = key;
-
-                if (sorted) {
-                    p->next = new ListNode(heap[0]->val);
-                    p = p->next;
-                } else {
-                    sorted = p = new ListNode(heap[0]->val);
-                }
-
-                key = heap[0]->next;
-                if (key == 0) {
-                    key = heap[--size];
-                }
-            }
-            return sorted;
         }
+        p->next = l1 ? l1 : l2;
+        return head->next;
+    }
+public:
+    ListNode *mergeKLists(vector<ListNode *> &lists) {
+        // IMPORTANT: Please reset any member data you declared, as
+        // the same Solution instance will be reused for each test case.
+        if (!lists.size()) return NULL;
+
+        ListNode *p = lists[0];
+        for (int i = 1; i < lists.size(); ++i) 
+            p = mergeTwoLists(p, lists[i]);
+        return p;
+    }
 };
